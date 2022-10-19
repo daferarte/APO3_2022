4889ecfcd288783473a6146267c924f5b0ba683f
Daniel Arteaga <daferarte@gmail.com>
2022-10-19 07:55:10 -0500

    Act automatica tabla

--- TiendaPokemon/src/MenuPrincipal/MenuPrincipal.java
+++ TiendaPokemon/src/MenuPrincipal/MenuPrincipal.java
@@ -122,7 +122,9 @@ public class MenuPrincipal extends javax.swing.JFrame {
 
     private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
         // TODO add your handling code here:
-        new MostrarPokemon().setVisible(true);
+        MostrarPokemon mosPokInterfaz = MostrarPokemon.getInstance();
+        mosPokInterfaz.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jMenuItem5ActionPerformed
 
     
--- TiendaPokemon/src/PokemonesVista/MostrarPokemon.java
+++ TiendaPokemon/src/PokemonesVista/MostrarPokemon.java
@@ -16,11 +16,24 @@ import pokemones.PokemonesController;
  */
 public class MostrarPokemon extends javax.swing.JFrame {
 
+    private static MostrarPokemon instancia=null;
     /**
      * Creates new form MostrarPokemon
      */
-    public MostrarPokemon() {
+    private MostrarPokemon() {
         initComponents();
+    }
+    
+    private void ActuListaPokemon(){
+        DAOpokemones pokCont = PokemonesController.getInstance();
+        jTable2.setModel(pokCont.MostrarPokemones());
+    }
+    
+    public static MostrarPokemon getInstance(){
+        if(instancia==null)
+            instancia=new MostrarPokemon();
+        instancia.ActuListaPokemon();
+        return instancia;
     }
 
     /**
@@ -35,6 +48,7 @@ public class MostrarPokemon extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable2 = new javax.swing.JTable();
@@ -45,10 +59,18 @@ public class MostrarPokemon extends javax.swing.JFrame {
         jLabel4.setText("Pokemones capturados");
         getContentPane().add(jLabel4, java.awt.BorderLayout.PAGE_START);
 
-        jPanel1.setLayout(new java.awt.GridLayout());
+        jPanel1.setLayout(new java.awt.GridLayout(1, 0));
 
         jButton1.setText("Regresar");
         jPanel1.add(jButton1);
+
+        jButton3.setText("Agregar pokemon");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton3);
 
         jButton2.setText("MostrarDatos");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -80,48 +102,22 @@ public class MostrarPokemon extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        DAOpokemones pokCont = PokemonesController.getInstance();
-        jTable2.setModel(pokCont.MostrarPokemones());
+        this.ActuListaPokemon();
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MostrarPokemon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MostrarPokemon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MostrarPokemon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MostrarPokemon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        AgregarPokemon interfaz = AgregarPokemon.getInstance();
+        interfaz.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MostrarPokemon().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane2;
